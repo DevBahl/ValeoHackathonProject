cad8c8c4a85fc8709e96564a0aeeb4807bc34f0d
Dev Bahl <devbahl12@gmail.com>
2020-02-06 15:59:23 +0530

    first commit

---
+++ app/src/main/java/com/dbsrm/valeohackathonproject/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.dbsrm.valeohackathonproject
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        sendbtn.setOnClickListener {
+            val intent = Intent(this,SpeechToTextRecog::class.java)
+            startActivity(intent)
+        }
+    }
+}
