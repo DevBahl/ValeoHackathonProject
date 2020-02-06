6240e3fa8dd6194195ee4631ab749494b91db794
Dev Bahl <devbahl12@gmail.com>
2020-02-06 15:58:32 +0530

    first commit

---
+++ app/src/main/java/com/dbsrm/valeohackathonproject/SpeechToTextRecog.kt
@@ -0,0 +1,51 @@
+package com.dbsrm.valeohackathonproject
+
+import android.app.Activity
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.speech.RecognizerIntent
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_speech_to_text_recog.*
+import java.util.*
+
+class SpeechToTextRecog : AppCompatActivity() {
+
+    private val REQUEST_CODE_SPEECH_INPUT = 100
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_speech_to_text_recog)
+
+        speechrecogbtn.setOnClickListener {
+            speak()
+        }
+    }
+
+    private fun speak() {
+        val mIntent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
+        mIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,RecognizerIntent.LANGUAGE_MODEL_FREE_FORM)
+        mIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault())
+        mIntent.putExtra(RecognizerIntent.EXTRA_PROMPT,"Speak Up Something!")
+
+        try {
+            startActivityForResult(mIntent,REQUEST_CODE_SPEECH_INPUT)
+
+        }catch (e: Exception){
+            Toast.makeText(this,e.message,Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int,resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode,resultCode, data)
+        when(requestCode){
+            REQUEST_CODE_SPEECH_INPUT ->{
+                if(resultCode == Activity.RESULT_OK && null != data){
+                    val result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS)
+                    recogText.text = result[0]
+
+                }
+            }
+        }
+    }
+}
